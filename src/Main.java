03701a1ab0f0db7f5a508a8b3aa022f22317fd1d
astra <diman.lan@Mail.ru>
2022-07-16 20:11:29 +0300

    For check

--- src/Main.java
+++ src/Main.java
@@ -41,62 +41,48 @@ public class Main {
         else {
             System.out.println(year + " год не является високосным!");
         }
+
 //4 task
         System.out.println("Введите расстояние до нас:");
         int deliveryDistance = Integer.parseInt(reader.readLine());
+        int deliveryDays = 1;
 
-        if (deliveryDistance < 20){
-            System.out.println("Потребуется дней: 1");
+
+        if (deliveryDistance > 20) {
+            deliveryDays++;
         }
-        else if (deliveryDistance < 60) {
-            System.out.println("Потребуется дней: 2");
-        }
-        else if (deliveryDistance < 100){
-            System.out.println("Потребуется дней: 3");
-        }
-        else {
-            System.out.println("Доставка осуществляется по почте. Срок не регламентирован.");
-        }
+        if (deliveryDistance > 60) {
+             deliveryDays++;
+         }
+        System.out.println("Потребуется дней: " + deliveryDays);
+
 //5th task
+
         System.out.println("Введите номер месяца:");
         int month = Integer.parseInt(reader.readLine());
 
         switch (month) {
             case 1:
-                System.out.println("Январь");
-                break;
             case 2:
-                System.out.println("Февраль");
+                System.out.println("Зима");
                 break;
             case 3:
-                System.out.println("Март");
-                break;
             case 4:
-                System.out.println("Апрель");
-                break;
             case 5:
-                System.out.println("Май");
+                System.out.println("Весна");
                 break;
             case 6:
-                System.out.println("Июнь");
-                break;
             case 7:
-                System.out.println("Июль");
-                break;
             case 8:
-                System.out.println("Август");
+                System.out.println("Лето");
                 break;
             case 9:
-                System.out.println("Сентябрь");
-                break;
             case 10:
-                System.out.println("Октябрь");
-                break;
             case 11:
-                System.out.println("Ноябрь");
+                System.out.println("Осень");
                 break;
             case 12:
-                System.out.println("Декабрь");
+                System.out.println("Зима");
                 break;
             default:
                 System.out.println("Такого не существует!");
